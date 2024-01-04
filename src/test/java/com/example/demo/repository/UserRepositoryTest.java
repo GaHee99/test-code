cd7b1a4b1e49bfad0aed6b514db841be8e11d1f4
gahee99 <u1879@sookmyung.ac.kr>
2024-01-04 15:23:48 +0900

    feat: 테스트 추가하기: h2를 이용한 repository 테스트

---
+++ src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -0,0 +1,81 @@
+package com.example.demo.repository;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import com.example.demo.model.UserStatus;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+
+
+@DataJpaTest(showSql = true)
+@Sql("/sql/user-repository-test-data.sql")
+@TestPropertySource("classpath:test-application.properties")
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+//    @Test
+//    void UserRepository_가_제대로_연결되었다() {
+//        //given
+//        UserEntity userEntity = new UserEntity();
+//        userEntity.setEmail("kdkd@gmail.com");
+//        userEntity.setAddress("seoul");
+//        userEntity.setNickname("가가");
+//        userEntity.setStatus(UserStatus.ACTIVE);
+//        userEntity.setCertificationCode("aaaaa-aaaaa-aaaaa");
+//
+//        //when
+//        UserEntity result = userRepository.save(userEntity);
+//
+//        //then
+//        assertThat(result.getId()).isNotNull();
+//    }
+
+    @Test
+    void findByIdAndStatus_로_유저데이터를_찾아올_수_있다() {
+        //given
+        //when
+        Optional<UserEntity> result = userRepository.findByIdAndStatus(1L, UserStatus.ACTIVE);
+
+        //then
+        assertThat(result.isPresent()).isTrue();
+    }
+
+    @Test
+    void findByIdAndStatus_는_데이터가_없으면_Optional_empty를_내려준다() {
+        //given
+        //when
+        Optional<UserEntity> result = userRepository.findByIdAndStatus(1L, UserStatus.PENDING);
+
+        //then
+        assertThat(result.isEmpty()).isTrue();
+    }
+
+
+    @Test
+    void findByEmailAndStatus_로_유저데이터를_찾아올_수_있다() {
+        //given
+        //when
+        Optional<UserEntity> result = userRepository.findByEmailAndStatus("kdkd@gmail.com", UserStatus.ACTIVE);
+
+        //then
+        assertThat(result.isPresent()).isTrue();
+    }
+
+    @Test
+    void findByEmailAndStatus_는_데이터가_없으면_Optional_empty를_내려준다() {
+        //given
+        //when
+        Optional<UserEntity> result = userRepository.findByEmailAndStatus("kdkd@gmail.com", UserStatus.PENDING);
+
+        //then
+        assertThat(result.isEmpty()).isTrue();
+    }
+
+
+}
