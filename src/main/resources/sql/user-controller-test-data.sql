a7e6bf9b604bef035cdbfff44bcc15f7235e0104
gahee99 <u1879@sookmyung.ac.kr>
2024-01-11 18:06:33 +0900

    feat: userController 테스트코드 작성

---
+++ src/main/resources/sql/user-controller-test-data.sql
@@ -0,0 +1,5 @@
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (1, 'kdkd@gmail.com', '가가', 'seoul', 'aaaaa-aaaaa-aaaaa', 'ACTIVE', 0);
+
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (2, 'kdkd2@gmail.com', '가가2', 'seoul', 'aaaaa-aaaaa-aaaab', 'ACTIVE', 0);
---
+++ src/test/java/com/example/demo/controller/HealthCheckTest.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+/**
+ * 컨트롤러 테스트를 위해서 mockMVC를 사용
+ * 주로 api테스트를 진행하는 데 많이 사용 됨
+ */
+@SpringBootTest
+@AutoConfigureMockMvc //MockMvc를 위한 자동 설정
+@AutoConfigureTestDatabase //MockMvc를 위한 자동 설정
+public class HealthCheckTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void 헬스_체크_응답이_200으로_내려온다() throws Exception {
+        mockMvc.perform(get("/health_check.html"))
+                .andExpect(status().isOk());
+    }
+}
